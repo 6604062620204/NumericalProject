1084538ae941abb85e23eff6ba8c92ac4121a1a1
6604062620204 <s6604062620204@email.kmutnb.ac.th>
2024-11-02 04:03:07 +0700

    main

--- src/api/root.js
+++ src/api/root.js
@@ -6,3 +6,5 @@ const url = 'http://localhost:3030/api/root';
 export const getData = async () => await axios.get(url);
 
 export const createData = async (data) => await axios.post(url, data);
+
+export const readsol = async (id) => await axios.get(`${url}/${id}`);
